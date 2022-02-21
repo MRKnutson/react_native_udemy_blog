464fa0586b643218451b56dfca32ea224bf4cc57
Michael Knutson <mrknutson44@gmail.com>
2022-02-20 20:27:07 -0900

    create screen broken out separate from index

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -11,11 +11,10 @@ import { Context as BlogsContext } from "../context/BlogsContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(BlogsContext);
+  const { state, deleteBlogPost } = useContext(BlogsContext);
 
   return (
     <View>
-      <Button title='Add Post' onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.id}
@@ -42,6 +41,16 @@ const IndexScreen = ({ navigation }) => {
       />
     </View>
   );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Feather name='plus' size={30} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
